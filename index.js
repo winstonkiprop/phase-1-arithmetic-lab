15b6292b608f19487cd3b996819a9e8354ce87e5
winston kiprop <winstonekip50@gmail.com>
2022-06-06 22:32:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10);
+let num3 = 26;
+let num4 = 11;
+let mod = num3 % num4;
+let max = Math.max(14, 12, 20);
